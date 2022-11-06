9f7c8b0dc10d721268ee06f991a47963ba48461c
Lucas Holten <lhc.holten@gmail.com>
2022-11-06 19:10:12 +0100

    Simplify the wrapper API

--- examples/atba.rs
+++ examples/atba.rs
@@ -1,23 +1,13 @@
 use quaternion::{conj, rotate_vector};
-use rlbot_bm::{
-    input::ControllerInput,
-    state::{GameState, Physics},
-};
+use rlbot_bm::{input::ControllerInput, state::Physics};
 use vecmath::vec3_normalized_sub;
 
 fn main() {
-    let mut framework = rlbot_bm::RlBotBm::new().expect("could not configure RLBot-BM");
-    let mut state = GameState::default();
+    let mut rlbot_bm = rlbot_bm::RlBotBm::new().expect("could not configure RLBot-BM");
     let mut input = ControllerInput::default();
     loop {
-        framework.next_state(&mut state);
-
-        if !state.is_round_active() {
-            continue;
-        }
-
-        let car = &state.cars()[framework.index];
-        let ball = &state.balls()[0];
+        let car = &rlbot_bm.state.cars()[rlbot_bm.index];
+        let ball = &rlbot_bm.state.balls()[0];
 
         let ball_direction = vec3_normalized_sub(ball.position(), car.position());
         let global_to_local = conj(car.orientation());
@@ -25,6 +15,6 @@ fn main() {
 
         input.steer = local_ball_direction[1].signum();
         input.throttle = 1.0;
-        framework.set_bot_input(&input)
+        rlbot_bm.update(&input)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,52 +2,59 @@ pub mod input;
 pub mod state;
 
 use std::{
-    cell::Cell,
     ffi::{c_int, c_ulong},
     fmt::Debug,
-    marker::PhantomData,
     ptr::NonNull,
 };
 
 use pico_args::Arguments;
-use rlbot_bm_sys::*;
 
 use crate::state::{CarId, GameState};
 
-// The PhantomData Cell is used to make the type !Sync
+/// This is the wrapper for RLBot-BM.
+/// All of the methods are blocking, so if you want to do computation while waiting,
+/// you should call these methods on a separate thread.
 pub struct RlBotBm {
-    handle: NonNull<RLBotBM_H>,
+    handle: NonNull<rlbot_bm_sys::RLBotBM_H>,
     pub index: CarId,
-    input: ControllerInput,
-    not_sync: PhantomData<Cell<()>>,
+    input: rlbot_bm_sys::ControllerInput,
+    pub state: GameState,
 }
 
 impl RlBotBm {
+    /// This creates a connectionto the RLBot-BM plugin and waits for a valid state
     pub fn new() -> Result<Self, impl Debug> {
         let mut args = Arguments::from_env();
         let player_index = args
             .value_from_str("--player-index")
-            .map_err(|_| "Failed to read player-index from args")?;
+            .map_err(|_| "Failed to read --player-index from args")?;
 
-        let unchecked_handle = unsafe { RLBotBM_create() };
+        let unchecked_handle = unsafe { rlbot_bm_sys::RLBotBM_create() };
         let handle =
             NonNull::new(unchecked_handle).ok_or("RLBot-BM handle could not be created")?;
 
-        Ok::<Self, &str>(Self {
+        let mut framework = Self {
             handle,
             index: CarId(player_index),
             input: Default::default(),
-            not_sync: PhantomData,
-        })
+            state: GameState::default(),
+        };
+
+        while !framework.state.is_round_active() {
+            framework.wait_next_tick();
+        }
+
+        Ok::<Self, &str>(framework)
     }
 
     /// returns whether it had to wait
-    pub fn next_state(&mut self, state: &mut GameState) -> bool {
+    fn wait_next_tick(&mut self) -> bool {
         let handle = self.handle.as_ptr();
-        unsafe { RLBotBM_pollNextTick(handle, &mut state.0) }
+        unsafe { rlbot_bm_sys::RLBotBM_waitNextTick(handle, &mut self.state.0) }
     }
 
-    pub fn set_bot_input(&mut self, input: &input::ControllerInput) {
+    /// Use this function inside a loop to set inputs for each tick
+    pub fn update(&mut self, input: &input::ControllerInput) {
         let handle = self.handle.as_ptr();
 
         self.input.throttle = input.throttle;
@@ -64,13 +71,15 @@ impl RlBotBm {
             self.input.itemTarget = item_target as c_ulong
         }
 
-        unsafe { RLBotBM_setBotInput(handle, &self.input, self.index.0 as c_int) }
+        unsafe { rlbot_bm_sys::RLBotBM_setBotInput(handle, &self.input, self.index.0 as c_int) }
+
+        self.wait_next_tick();
     }
 }
 
 impl Drop for RlBotBm {
     fn drop(&mut self) {
         let handle = self.handle.as_ptr();
-        unsafe { RLBotBM_destroy(handle) };
+        unsafe { rlbot_bm_sys::RLBotBM_destroy(handle) };
     }
 }
